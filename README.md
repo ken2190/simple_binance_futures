9bafa66f3fcc88f9d543d9954ad0b94a9da3b086
dingzhoufeng <ding.zhoufeng@wuta-camera.com>
2021-08-20 12:39:01 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# simple_binance_futures
-Binance leverage futures Hook
+# Simple binance futures
+
+## Attention
+
+- Only support **binance**
+- Only support **isolated**
+- Only **long**, Short is not supported
+- Not support **increasing margin**
+- Not support **adjusting leverage** when holding positions,This is a limitation of Binance
+- Please use the **2021.07** version of FT. There is no guarantee that the previous or later versions will support.Don't forget `pip install -r requirements.txt` after updating FT
+
+## Prepare
+- New binance users only support 20x leverage, please confirm whether your account supports higher leverage!
+Make sure your API is enabled and have Futures permissions
+Open futures after created the API, the created API still does not support futres, you must create a new API
+- When the script starts, it will initialize Leverage, PositionsideDual, Isolated, so your position needs to be empty
+- Make sure that the following methods are not customized `custom_stake_amount` `custom_stoploss` `confirm_trade_entry` `bot_loop_start`
+
+## Modify config.json
+- Must set `order_types.stoploss_on_exchange` to `true`.
+- Muset set `bid_strategy.use_order_book` to `true`.
+- Muset set `ask_strategy.use_order_book` to `true`.
+- `exchange.ccxt_config` add `"options": {"defaultType": "future"}`
+- `exchange.ccxt_async_config` add `"options": {"defaultType": "future"}`
+
+
+## Modify strategy
+```
+from pandas import DataFrame
+
+from freqtrade.strategy import stoploss_from_open
+from pathlib import Path
+import sys
+
+sys.path.append(str(Path(__file__).parent))
+import importlib
+
+interface_futures_binance = importlib.import_module("interface_futures_binance")
+importlib.reload(interface_futures_binance)
+
+
+class SimpleBinanceFutures(interface_futures_binance.IFutures):
+    use_custom_stoploss = True
+
+    # Futures config
+    # New binance users only support 20x leverage, please confirm whether your account supports higher leverage!
+    # Make sure your API is enabled and have Futures permissions
+    # (open futures after created the API, the created API still does not support futres, you must create a new API)
+    _leverage = 10
+
+    def custom_stoploss(
+        self, pair: str, trade: 'Trade', current_time: 'datetime', current_rate: float, current_profit: float, **kwargs
+    ) -> float:
+        """
+        Must! Must! Must use custom_stoploss, and turn on stoploss_on_exchange
+
+        FT spot does not handle the logic of forced liquidation.
+        Therefore, stoploss_on_exchange must be used to manually liquidate the position before the forced liquidation
+        """
+        p = locals()
+        del p["self"], p["__class__"]
+        parent_stoploss = super().custom_stoploss(**p)
+
+        return max(stoploss_from_open(parent_stoploss, current_profit), -1)
+```
+
+## Other
+- If you want to change the strategy, it is recommended to **restart** instead of using **web reload**.
---
+++ user_data/strategies/interface_futures_binance.py
@@ -0,0 +1,253 @@
+import ccxt
+import arrow
+import logging
+from typing import Dict
+from freqtrade.exchange import Exchange, Binance
+from freqtrade.wallets import Wallets, Wallet
+from freqtrade.persistence import LocalTrade, Trade
+from freqtrade.exchange.common import retrier
+from freqtrade.exceptions import (
+    DDosProtection, InsufficientFundsError, ExchangeError, InvalidOrderException, TemporaryError, OperationalException
+)
+from freqtrade.strategy.interface import IStrategy
+from freqtrade.strategy import stoploss_from_open
+from datetime import datetime, timedelta
+from freqtrade.enums import RunMode
+from freqtrade.constants import UNLIMITED_STAKE_AMOUNT
+
+logger = logging.getLogger(__name__)
+
+
+class IFutures(IStrategy):
+    _initialized_futures: bool = False
+    _leverage: int = 1
+    _isolated: bool = True
+    _maintenance_stoploss: Dict = {}
+
+    def custom_stake_amount(
+        self, pair: str, current_time: datetime, current_rate: float, proposed_stake: float, min_stake: float,
+        max_stake: float, **kwargs
+    ) -> float:
+
+        if self.config['stake_amount'] == UNLIMITED_STAKE_AMOUNT:
+            return proposed_stake
+        else:
+            return proposed_stake * self._leverage
+
+    def custom_stoploss(self, pair: str, trade: 'Trade', current_time: datetime,
+                        current_rate: float, current_profit: float, **kwargs) -> float:
+        p = locals()
+        del p["self"], p["__class__"]
+        stoploss = super().custom_stoploss(**p)
+
+        if pair in self._maintenance_stoploss:
+            stoploss = self._maintenance_stoploss[pair]
+
+        return stoploss
+
+    def confirm_trade_entry(
+        self, pair: str, order_type: str, amount: float, rate: float, time_in_force: str, current_time: datetime,
+        **kwargs
+    ) -> bool:
+        if self.dp.runmode not in [RunMode.LIVE]:
+            self._maintenance_stoploss[pair] = self.stoploss
+
+        if pair not in self._maintenance_stoploss:
+            self.reset_leverage()
+
+        return pair in self._maintenance_stoploss
+
+    def check_config(self):
+        if self._leverage < 1:
+            raise OperationalException(f'Leverage must be grater than 0')
+
+        if "edge" in self.config and "enabled" in self.config["edge"] and self.config["edge"]["enabled"]:
+            raise OperationalException('Futures does not support edge')
+
+        if self.config["exchange"]["name"] != "binance":
+            raise OperationalException(f'Future only supports binance')
+
+    def hook_method(self):
+        Exchange.get_balances = get_balances
+        Exchange.leverage = self._leverage
+        Exchange.isolated = self._isolated
+        Binance._params = {"workingType": "MARK_PRICE"}
+        Binance.stoploss_adjust = stoploss_adjust
+        Binance.stoploss = stoploss
+        Wallets.get_free = get_free
+        Wallets._update_dry = _update_dry
+        Wallets.leverage = self._leverage
+        Wallets.isolated = self._isolated
+        logger.info('Hook spot method!')
+
+    def reset_stoploss(self):
+        default_maintenance = 0 if self.dp.runmode in [RunMode.LIVE] else 0.03
+        self.stoploss = max(self.stoploss, -1 / self._leverage + default_maintenance)
+        logger.info(f'Reset stoploss to {self.stoploss}!')
+
+    def reset_leverage(self):
+        logger.info(f'Reset leverage and isolated and dual side!')
+
+        self.wallets._exchange._api.fapiPrivateGetPositionsideDual(params={"dualSidePosition": False})
+
+        for x in self.wallets._exchange._api.fetch_positions():
+            stoploss = self.stoploss + x["maintenanceMarginPercentage"]
+            ratio = self.order_types.get('stoploss_on_exchange_limit_ratio', 0.99)
+            self._maintenance_stoploss[x["symbol"]] = (stoploss + 1) / ratio - 1
+
+        for x in self.wallets._exchange._api.fetch_balance()["info"]["positions"]:
+            if int(x["leverage"]) != self._leverage:
+                logger.info('set leverage %s. leverage: %s.', x["symbol"], self._leverage)
+                self.wallets._exchange._api.set_leverage(
+                    self.wallets._exchange._api.markets_by_id[x["symbol"]]["symbol"],
+                    self._leverage,
+                )
+            if x["isolated"] != self._isolated:
+                logger.info('set isolated %s. isolated: %s.', x["symbol"], "ISOLATED" if self._isolated else "CROSSED")
+                self.wallets._exchange._api.set_margin_mode(
+                    self.wallets._exchange._api.markets_by_id[x["symbol"]]["symbol"],
+                    "ISOLATED" if self._isolated else "CROSSED",
+                )
+
+        self.wallets._exchange.reload_markets()
+
+    def bot_loop_start(self, **kwargs) -> None:
+        if self._initialized_futures:
+            return
+
+        self.check_config()
+
+        self.hook_method()
+
+        self.reset_stoploss()
+
+        if self.dp.runmode in [RunMode.LIVE]:
+            self.reset_leverage()
+
+        self._initialized_futures = True
+
+
+# ------------------------------------------------------------------------
+def get_free(self, currency: str) -> float:
+    leverage = self.leverage if currency == self._config['stake_currency'] else 1
+
+    balance = self._wallets.get(currency)
+    if balance and balance.free:
+        return balance.free * leverage
+    else:
+        return 0
+
+def _update_dry(self) -> None:
+
+    _wallets = {}
+    open_trades = Trade.get_trades_proxy(is_open=True)
+    # If not backtesting...
+    # TODO: potentially remove the ._log workaround to determine backtest mode.
+    if self._log:
+        tot_profit = Trade.get_total_closed_profit()
+    else:
+        tot_profit = LocalTrade.total_profit
+    tot_in_trades = sum([trade.stake_amount for trade in open_trades])
+
+    current_stake = self.start_cap + tot_profit - tot_in_trades / self.leverage
+    _wallets[self._config['stake_currency']] = Wallet(self._config['stake_currency'], current_stake, 0, current_stake)
+
+    for trade in open_trades:
+        curr = self._exchange.get_pair_base_currency(trade.pair)
+        _wallets[curr] = Wallet(curr, trade.amount, 0, trade.amount)
+    self._wallets = _wallets
+
+
+@retrier
+def get_balances(self) -> dict:
+
+    try:
+        balances = self._api.fetch_balance()
+        if "positions" in balances["info"]:
+            for x in balances["info"]["positions"]:
+                if x["symbol"] not in self._api.markets_by_id:
+                    continue
+
+                quote = self._api.markets_by_id[x["symbol"]]["quote"]
+                if quote != self._config["stake_currency"]:
+                    continue
+
+                base = self._api.markets_by_id[x["symbol"]]["base"]
+                balances[base] = {"free": float(x["positionAmt"]), "used": 0, "total": float(x["positionAmt"])}
+
+        # Remove additional info from ccxt results
+        balances.pop("info", None)
+        balances.pop("free", None)
+        balances.pop("total", None)
+        balances.pop("used", None)
+
+        return balances
+    except ccxt.DDoSProtection as e:
+        raise DDosProtection(e) from e
+    except (ccxt.NetworkError, ccxt.ExchangeError) as e:
+        raise TemporaryError(f'Could not get balance due to {e.__class__.__name__}. Message: {e}') from e
+    except ccxt.BaseError as e:
+        raise OperationalException(e) from e
+
+
+def stoploss_adjust(self, stop_loss: float, order: Dict) -> bool:
+    return order['type'] == 'stop' and stop_loss > float(order['info']['stopPrice'])
+
+
+@retrier(retries=0)
+def stoploss(self, pair: str, amount: float, stop_price: float, order_types: Dict) -> Dict:
+    """
+    creates a stoploss limit order.
+    this stoploss-limit is binance-specific.
+    It may work with a limited number of other exchanges, but this has not been tested yet.
+    """
+    # Limit price threshold: As limit price should always be below stop-price
+    limit_price_pct = order_types.get('stoploss_on_exchange_limit_ratio', 0.99)
+    rate = stop_price * limit_price_pct
+
+    ordertype = "stop"
+
+    stop_price = self.price_to_precision(pair, stop_price)
+
+    # Ensure rate is less than stop price
+    if stop_price <= rate:
+        raise OperationalException('In stoploss limit order, stop price should be more than limit price')
+
+    if self._config['dry_run']:
+        dry_order = self.create_dry_run_order(pair, ordertype, "sell", amount, stop_price)
+        return dry_order
+
+    try:
+        params = self._params.copy()
+        params.update({'stopPrice': stop_price})
+
+        amount = self.amount_to_precision(pair, amount)
+
+        rate = self.price_to_precision(pair, rate)
+
+        order = self._api.create_order(
+            symbol=pair, type=ordertype, side='sell', amount=amount, price=rate, params=params
+        )
+        logger.info('stoploss limit order added for %s. ' 'stop price: %s. limit: %s', pair, stop_price, rate)
+        self._log_exchange_response('create_stoploss_order', order)
+        return order
+    except ccxt.InsufficientFunds as e:
+        raise InsufficientFundsError(
+            f'Insufficient funds to create {ordertype} sell order on market {pair}. '
+            f'Tried to sell amount {amount} at rate {rate}. '
+            f'Message: {e}'
+        ) from e
+    except ccxt.InvalidOrder as e:
+        # Errors:
+        # `binance Order would trigger immediately.`
+        raise InvalidOrderException(
+            f'Could not create {ordertype} sell order on market {pair}. '
+            f'Tried to sell amount {amount} at rate {rate}. '
+            f'Message: {e}'
+        ) from e
+    except ccxt.DDoSProtection as e:
+        raise DDosProtection(e) from e
+    except (ccxt.NetworkError, ccxt.ExchangeError) as e:
+        raise TemporaryError(f'Could not place sell order due to {e.__class__.__name__}. Message: {e}') from e
+    except ccxt.BaseError as e:
+        raise OperationalException(e) from e
